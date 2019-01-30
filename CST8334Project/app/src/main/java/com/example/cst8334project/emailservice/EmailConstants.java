84c3f4a39e3b6a548b408568c5ec9efae75a4680
Sankalp Pandey <pand0041@algonquinlive.com>
2019-01-29 20:26:32 -0500

    Added class containing constants pertaining to sending emails.

---
+++ CST8334Project/app/src/main/java/com/example/cst8334project/emailservice/EmailConstants.java
@@ -0,0 +1,47 @@
+package com.example.cst8334project.emailservice;
+
+import java.util.Properties;
+
+/**
+ * This class defines several constants pertaining to sending emails.
+ */
+public final class EmailConstants {
+
+    /**
+     * The email address that will be used by all volunteers to send emails to the Heart House
+     * Hospice.
+     */
+    public static final String SENDER_EMAIL_ADDRESS = "email";
+
+    /**
+     * The password corresponding to the email address that will be used by all volunteers to send
+     * emails to the Heart House Hospice.
+     */
+    public static final String SENDER_EMAIL_PASSWORD = "password";
+
+    /**
+     * The receiving email address belonging to the Heart House Hospice.
+     */
+    public static final String RECIPIENT_EMAIL_ADDRESS = "abc123@hearthousehospice.com";
+
+    /**
+     * The SMTP properties that facilitate sending emails.
+     */
+    public static final Properties EMAIL_PROPERTIES;
+
+    // Set the STMP properties using a static initializer block.
+    static {
+        EMAIL_PROPERTIES = new Properties();
+        EMAIL_PROPERTIES.put("mail.smtp.host", "smtp.gmail.com");
+        EMAIL_PROPERTIES.put("mail.smtp.socketFactory.port", "465");
+        EMAIL_PROPERTIES.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        EMAIL_PROPERTIES.put("mail.smtp.auth", "true");
+        EMAIL_PROPERTIES.put("mail.smtp.port", "465");
+    }
+
+    /**
+     * Prevent instantiation.
+     */
+    private EmailConstants() {
+    }
+}
