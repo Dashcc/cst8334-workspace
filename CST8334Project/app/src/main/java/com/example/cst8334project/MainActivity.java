697715e5f43e96193aab0a62c4e92d7f4b66494e
Sankalp Pandey <pand0041@algonquinlive.com>
2019-02-06 09:26:26 -0500

    Added functionality to add attachment to emails, and logging sent emails.

--- CST8334Project/app/src/main/java/com/example/cst8334project/MainActivity.java
+++ CST8334Project/app/src/main/java/com/example/cst8334project/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.cst8334project;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.example.cst8334project.emailservice.Email;
 import com.example.cst8334project.emailservice.SendEmailActivity;
+import com.example.cst8334project.util.FileUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new SendEmailActivity(this, "Test email", "Test body").execute();
+
+        Email email = new Email();
+        email.setSubject("Subject");
+        email.setBody("Body");
+        email.setCsvAttachmentAbsolutePath(this.getFilesDir().getPath() + "/Data.csv");
+        email.setCsvAttachmentFileName("Information for Volunteer John");
+
+        new SendEmailActivity(this, email).execute();
     }
 }
--- CST8334Project/app/src/main/java/com/example/cst8334project/emailservice/SendEmailActivity.java
+++ CST8334Project/app/src/main/java/com/example/cst8334project/emailservice/SendEmailActivity.java
@@ -4,17 +4,16 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.example.cst8334project.util.FileUtils;
+
 import java.lang.ref.WeakReference;
+import java.util.Date;
+
+import javax.activation.*;
+import javax.mail.*;
+import javax.mail.internet.*;
 
 import static com.example.cst8334project.emailservice.EmailConstants.*;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 
 /**
  * This class is responsible for sending emails in a background thread.
@@ -27,28 +26,21 @@ public class SendEmailActivity extends AsyncTask<Void, Void, Void> {
     private final WeakReference<Context> context;
 
     /**
-     * The subject of the email to be sent.
+     * The email to be sent.
      */
-    private final String emailSubject;
-
-    /**
-     * The body of the email to be sent.
-     */
-    private final String emailBody;
+    private final Email email;
 
     /**
      * All-args constructor.
      *
-     * @param context      the {@link Context} of the application; can be provided to this
-     *                     constructor by setting 'this' as a parameter from a class
-     *                     that extends {@link android.app.Activity}
-     * @param emailSubject the subject of the email to be sent
-     * @param emailBody    the body of the email to be sent
+     * @param context the {@link Context} of the application; can be provided to this
+     *                constructor by setting 'this' as a parameter from a class
+     *                that extends {@link android.app.Activity}
+     * @param email   the email to be sent
      */
-    public SendEmailActivity(Context context, String emailSubject, String emailBody) {
-        this.context = new WeakReference<Context>(context);
-        this.emailSubject = emailSubject;
-        this.emailBody = emailBody;
+    public SendEmailActivity(Context context, Email email) {
+        this.context = new WeakReference<>(context);
+        this.email = email;
     }
 
     @Override
@@ -74,29 +66,67 @@ public class SendEmailActivity extends AsyncTask<Void, Void, Void> {
 
         try {
             // Create the MimeMessage object using the provided Session
-            MimeMessage mm = new MimeMessage(session);
+            MimeMessage mimeMessage = new MimeMessage(session);
 
             // Set the sender for the email
-            mm.setFrom(new InternetAddress(SENDER_EMAIL_ADDRESS));
+            mimeMessage.setFrom(new InternetAddress(SENDER_EMAIL_ADDRESS));
 
             // Set the recipient for the email
-            mm.addRecipient(Message.RecipientType.TO, new InternetAddress(RECIPIENT_EMAIL_ADDRESS));
+            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(RECIPIENT_EMAIL_ADDRESS));
 
             // Set the subject for the email
-            mm.setSubject(emailSubject);
+            mimeMessage.setSubject(email.getSubject());
+
+            // Create the email body part
+            MimeBodyPart messageBodyPart = new MimeBodyPart();
 
             // Set the body of the email
-            mm.setText(emailBody);
+            messageBodyPart.setText(email.getBody());
+
+            // Create a multipart message
+            Multipart multipart = new MimeMultipart();
+
+            // Set the text message part
+            multipart.addBodyPart(messageBodyPart);
+
+            // Create the email attachment part
+            MimeBodyPart messageAttachmentPart = new MimeBodyPart();
+            DataSource dataSource = new FileDataSource(email.getCsvAttachmentFileName());
+            messageAttachmentPart.setDataHandler(new DataHandler(dataSource));
+            messageAttachmentPart.setFileName(email.getCsvAttachmentFileName());
+            multipart.addBodyPart(messageAttachmentPart);
+
+            // Set the body part and the attachment part as a multipart
+            mimeMessage.setContent(multipart);
 
             // Send the email
-            Transport.send(mm);
+            Transport.send(mimeMessage);
 
-            Log.i("SendMailActivity", "Email sent.");
+            // Construct the logging message
+            String loggingMessage = String.format("%n Email sent from: %s %n" +
+                                                  "to: %s %n" +
+                                                  "with subject: %s %n" +
+                                                  "body: %s %n" +
+                                                  "attachment file name: %s %n" +
+                                                  "attachment body: %n" +
+                                                  "%s %n" +
+                                                  "Sent at %s.",
+                                                  SENDER_EMAIL_ADDRESS,
+                                                  RECIPIENT_EMAIL_ADDRESS,
+                                                  email.getSubject(),
+                                                  email.getBody(),
+                                                  email.getCsvAttachmentFileName(),
+                                                  FileUtils.readTextFromFile(email.getCsvAttachmentAbsolutePath()),
+                                                  new Date().toString());
+
+            // Log the sent email
+            Log.i("SendEmailActivity", loggingMessage);
 
         } catch (MessagingException e) {
-            Log.e("SendMailActivity", "Error occurred while attempting to send email.", e);
+            Log.e("SendEmailActivity", "Error occurred while attempting to send email.", e);
         }
 
         return null;
     }
+
 }
