e51a1807e8f2a8c1be6f88852e364a010acc1f89
Sankalp Pandey <pand0041@algonquinlive.com>
2019-02-09 07:38:29 -0500

    Added Visit Database and Visit DAO.

---
+++ CST8334Project/app/src/main/java/com/example/cst8334project/userhistoryservice/VisitDAO.java
@@ -0,0 +1,80 @@
+package com.example.cst8334project.userhistoryservice;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+/**
+ * Provides CRUD functionalities for {@link Visit}s.
+ */
+@Dao
+public interface VisitDAO {
+
+    /**
+     * Insert the given {@link Visit} into the data store.
+     *
+     * @param visit the {@link Visit} to insert into the data store.
+     * @return the id of the inserted {@link Visit}
+     */
+    @Insert
+    long insertVisit(Visit visit);
+
+    /**
+     * Find all {@link Visit}s in descending order of created date.
+     *
+     * @return List of all {@link Visit}s in descending order of created date
+     */
+    @Query("SELECT * FROM visit ORDER BY created_at DESC")
+    List<Visit> findAllVisits();
+
+    /**
+     * Delete the given {@link Visit} from the data store.
+     *
+     * @param visit the {@link Visit} to delete from the data store
+     */
+    @Delete
+    void deleteVisit(Visit visit);
+
+    /**
+     * Delete the {@link Visit} identified by the given id from the data store.
+     *
+     * @param visitId the id of the {@link Visit} to delete from the data store.
+     */
+    @Query("DELETE FROM visit WHERE id = :visitId")
+    void deleteVisitById(int visitId);
+
+    /**
+     * Update the given {@link Visit} in the data store.
+     *
+     * @param visit the {@link Visit} to update
+     */
+    @Update
+    void updateVisit(Visit visit);
+
+    /**
+     * Find the number of {@link Visit}s in the data store.
+     *
+     * @return the number of {@link Visit}s in the data store
+     */
+    @Query("SELECT count(*) FROM visit")
+    int visitCount();
+
+    /**
+     * Find the {@link Visit} identified by the given unique id.
+     *
+     * @param visitId the id of the {@link Visit} to retrieve
+     * @return the {@link Visit} identified by the given unique id if it exists, null otherwise
+     */
+    @Query("SELECT * FROM visit WHERE id = :visitId")
+    Visit findVisitById(int visitId);
+
+    /**
+     * Delete all {@link Visit}s from the data store.
+     */
+    @Query("DELETE FROM visit")
+    void deleteAllVisits();
+}
---
+++ CST8334Project/app/src/main/java/com/example/cst8334project/userhistoryservice/VisitDatabase.java
@@ -0,0 +1,59 @@
+package com.example.cst8334project.userhistoryservice;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+import android.content.Context;
+
+import com.example.cst8334project.util.Converters;
+
+/**
+ * The database for storing, retrieving, and modifying {@link Visit}s.
+ */
+@Database(entities = {Visit.class}, version = 1, exportSchema = false)
+@TypeConverters({Converters.class})
+public abstract class VisitDatabase extends RoomDatabase {
+
+    /**
+     * The name of the database.
+     */
+    private static final String DATABASE_NAME = "VISITS_DATABASE.db";
+
+    /**
+     * The singleton database instance.
+     */
+    private static volatile VisitDatabase instance;
+
+    /**
+     * Get the singleton instance of the {@link VisitDatabase}.
+     *
+     * @param context the {@link Context} for the application
+     * @return
+     */
+    public static synchronized VisitDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = createDatabase(context);
+        }
+        return instance;
+    }
+
+    /**
+     * Create the database using the given {@link Context}.
+     *
+     * @param context the {@link Context} for the application
+     * @return the fully constructed {@link VisitDatabase} instance
+     */
+    private static VisitDatabase createDatabase(Context context) {
+        return Room.databaseBuilder(context, VisitDatabase.class, DATABASE_NAME)
+                .allowMainThreadQueries()
+                .build();
+    }
+
+    /**
+     * Get the {@link VisitDAO} object used to execute CRUD statements for {@link Visit}s.
+     *
+     * @return the {@link VisitDAO} object
+     */
+    public abstract VisitDAO getVisitDAO();
+}
